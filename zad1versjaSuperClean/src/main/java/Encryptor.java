47d9747d335ba2a813beddfe1fef93687fb61359
Weronika Kowalkowska <wkow19@gmail.com>
2025-04-02 20:59:09 +0200

    licencja

--- zad1versjaSuperClean/src/main/java/Encryptor.java
+++ zad1versjaSuperClean/src/main/java/Encryptor.java
@@ -1,7 +1,24 @@
+/*
+    AES Encryption and Decryption application
+    Copyright (C) 2025  Weronika Kowalkowska 251561, Nadzeya Silchankava 253184
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import javax.crypto.KeyGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-
 
 public class Encryptor {
     private byte[] plainBytes;                  //tekst jawny zamieniony na bajty
